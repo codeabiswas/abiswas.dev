7d074dbda9136c4d620f1933b39319828a8d5df7
codeabiswas <axb6972@rit.edu>
2023-10-25 21:56:38 +0530

    Cleaned up code a bit

--- src/sections/skills.js
+++ src/sections/skills.js
@@ -17,20 +17,6 @@ import {
 const Skills = ({ rawContent }) => {
   const content = (
     <>
-      {/* {rawContent.skills.map((skill, index) => (
-        <Fragment key={index}>
-          {Object.keys(skill).map((category) => (
-            <Fragment key={category}>
-              <h6>{category}</h6>
-              <ul>
-                {Object.values(skill).map((category) =>
-                  category.map((item) => <li key={item}>{item}</li>)
-                )}
-              </ul>
-            </Fragment>
-          ))}
-        </Fragment>
-      ))} */}
       <StyledResponsiveMasonry>
         <Masonry>
           {rawContent.skills.map((skill, index) => (
