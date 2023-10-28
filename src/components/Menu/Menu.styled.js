9e85caae63e8402670c5ee2663216572861ab1ad
codeabiswas <axb6972@rit.edu>
2023-10-28 13:39:14 +0530

    Added navbar styling and changed theme toggle

--- src/components/Menu/Menu.styled.js
+++ src/components/Menu/Menu.styled.js
@@ -5,7 +5,9 @@ const StyledMenu = styled(Navbar).attrs({
   className: "py-3 bg-body-tertiary",
   expand: "lg",
   fixed: "top",
-})``;
+})`
+  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.2);
+`;
 
 const CollapsibleNavbar = styled(Navbar.Collapse).attrs({
   className: "d-flex-md flex-column align-self-center",
