f8031e10f8f388b7aba4ad57f3fc40d7a76890c0
codeabiswas <axb6972@rit.edu>
2024-07-01 22:14:44 +0530

    Cleaned up the project a bit based on files not active

--- src/components/Section/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const StyledSection = styled.section.attrs({
-  className: "min-vh-100 d-flex flex-column justify-content-center",
-})``;
-
-export default StyledSection;
